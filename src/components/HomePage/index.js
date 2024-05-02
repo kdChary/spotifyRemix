03a8e5e463f6bcacc1ad45d6edfacd3e06fd685f
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-05-02 14:57:52 +0000

    HomePage 26 cases passed

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,16 +1,19 @@
 import './index.css'
 import Header from '../Header'
 import HomeEditorPicks from '../HomeEditors'
-// import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar'
+import HomeCategories from '../HomeCategories'
+import HomeNewReleases from '../HomeNewReleases'
 
 const HomePage = () => (
   <>
     <Header />
     <div className="home-page" data-testid="homePage">
+      <Sidebar />
       <div className="home-items">
         <HomeEditorPicks />
-        <h3>Categories</h3>
-        <h3>New Releases</h3>
+        <HomeCategories />
+        <HomeNewReleases />
       </div>
     </div>
   </>
