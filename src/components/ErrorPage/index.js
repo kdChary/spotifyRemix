33341bebabe6f16f53cd10896cde09b4153fd898
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-05-03 02:27:53 +0000

    alert image added

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -12,7 +12,11 @@ const Failure = props => {
   return (
     <div className="failure-view" data-testid="failureView">
       <div className="failure-content">
-        <BsExclamationTriangleFill className="failure-icon" />
+        <img
+          src="https://res.cloudinary.com/dgga8cymk/image/upload/v1712921765/1Spotify/alert.png"
+          alt="failure view"
+          className="failure-icon"
+        />
         <p className="failure-text">Something went wrong. Please try again</p>
         <button className="failure-btn" type="button" onClick={btnClicked}>
           Try Again
