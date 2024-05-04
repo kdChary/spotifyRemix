ef3bd125da0ac4d44144e8d318b81279c874be09
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-05-04 11:20:57 +0000

    audio player added

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,16 +1,40 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/media-has-caption */
+// import {useState, useRef} from 'react'
+import {FaPlay, FaPause} from 'react-icons/fa'
+
 import './index.css'
 
 const AudioPlayer = props => {
+  //   const [playing, togglePlay] = useState(false)
+
   const {trackData, image} = props
-  const {trackName, trackImage, trackArtist, previewUrl, duration} = trackData
+  const {trackName, trackImage, trackArtist, previewUrl} = trackData
   const imgSrc = trackImage !== undefined ? trackImage : image
+
+  //   const audioPlayer = useRef()
+
+  //   const togglePlayPause = () => {
+  //     togglePlay(!playing)
+  //   }
 
   const playerSection = () => (
     <div className="player-section">
-      <audio src={previewUrl} controls preload="metadata" />
+      <audio
+        src={previewUrl}
+        preload="metadata"
+        controls
+        className="music-player"
+      />
+      {/* <div className="music-controls">
+        <button className="play-pause-btn" onClick={() => togglePlayPause()}>
+          {playing ? <FaPause /> : <FaPlay />}
+        </button>
+        <input type="range" name="music" className="progressbar" />
+      </div> */}
     </div>
   )
+
   return (
     <div className="player-container">
       <div className="left-section">
