5ea5bf3163f12ec94a47270be181fc3a09122f45
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-05-05 15:03:53 +0000

    made responsive

--- src/components/PlaylistDetailsItem/index.js
+++ src/components/PlaylistDetailsItem/index.js
@@ -10,27 +10,35 @@ const PlaylistItem = props => {
       data-testid="specificPlaylistItem"
       onClick={() => playSong(trackData)}
     >
-      <div className="hash">
+      <div className="hash large">
         <p>{no + 1}</p>
       </div>
-      <div className="playlist-item-div">
+      <div className="playlist-item-div large">
         <p className="playlist-item-data">{trackName}</p>
       </div>
 
-      <div className="playlist-item-div">
+      <div className="playlist-item-div large">
         <p className="playlist-item-data">{trackAlbum}</p>
       </div>
 
-      <div className="playlist-item-div ">
+      <div className="playlist-item-div large">
         <p className="playlist-item-data">{duration}</p>
       </div>
 
-      <div className="playlist-item-div">
+      <div className="playlist-item-div large">
         <p className="playlist-item-data">{trackArtist}</p>
       </div>
 
-      <div className="playlist-item-div">
+      <div className="playlist-item-div large">
         <p className="playlist-item-data">{addedAt}</p>
+      </div>
+
+      <div className="small">
+        <div>
+          <p className="song">{trackName}</p>
+          <p className="artist">{trackArtist}</p>
+        </div>
+        <p className="duration">{duration}</p>
       </div>
     </li>
   )
